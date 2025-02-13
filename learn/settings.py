ae36b36dd88bf23459dea68c65343b72834b5585
munubi <sspirial@gmail.com>
2025-02-13 08:28:15 +0300

    Enable DEBUG mode for development and add Vercel configuration

--- learn/settings.py
+++ learn/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tgwo@s^gpc+kv+pryzcne-v*upc=%12b_bt)r!dkwbz1x+h80n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
